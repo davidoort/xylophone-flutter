8c566a35bfbb6d097ba8ef642eb32277507e76c3
David Oort Alonso <davidoort@hotmail.com>
2020-04-03 17:02:19 +0200

    tested on hardware iphone

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,9 +8,41 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              XyloNote(noteNumber: '1'),
+              XyloNote(noteNumber: '2'),
+              XyloNote(noteNumber: '3'),
+              XyloNote(noteNumber: '4'),
+              XyloNote(noteNumber: '5'),
+              XyloNote(noteNumber: '6'),
+              XyloNote(noteNumber: '7'),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class XyloNote extends StatelessWidget {
+  final String noteNumber;
+  XyloNote({@required this.noteNumber});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: FlatButton(
+        padding: EdgeInsets.zero,
+        color: Colors.blue[900 - int.parse(noteNumber) * 100],
+        child: null,
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note$noteNumber.wav');
+        },
       ),
     );
   }
